2d95942964770949e671a3a753f974e3d4f7b255
makoto-niko <mmana19970613@gmail.com>
2024-04-07 11:39:56 +0000

    [Change] Migrations

--- db/migrate/20240401043843_create_admins.rb
+++ db/migrate/20240401043843_create_admins.rb
@@ -1,14 +1,12 @@
 class CreateAdmins < ActiveRecord::Migration[6.1]
-  def change
-     create_table :admins do |t|
-      t.string :username, null: false
-      t.string :email, null: false
-      t.datetime :created_at, precision: 6, null: false
-      t.datetime :updated_at, precision: 6, null: false
+    def change
+        create_table :admins do |t|
+            t.string :username, null: false
+            t.string :email, null: false
+            t.datetime :created_at, precision: 6, null: false
+            t.datetime :updated_at, precision: 6, null: false
+        end
     end
-    
-    add_index :admins, :reset_password_token, unique: true
-    end
-  end
+end
 
 
