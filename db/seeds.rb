6981c65aea0dfb2cc66957fb448881fe5d5e2304
makoto-niko <mmana19970613@gmail.com>
2024-04-21 06:38:35 +0000

    検索ボックス修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,4 @@
-Admin.create!(
-  username: 'admin_user',
-  email: 'aa@aa',
-  password: 'aaaaaaaa',
-  password_confirmation: 'aaaaaaaa'
-)
+
 
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
