d17668c3a5040551315a6a4236f0aed44a4a4eb8
makoto-niko <mmana19970613@gmail.com>
2024-04-25 10:22:15 +0000

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,20 +1,20 @@
 users_data = [
-  { name: '佐藤 健太', password: 'KentaSato123', email: 'kenta.sato@example.com' },
-  { name: '鈴木 花子', password: 'HanakoSuzuki456', email: 'hanako.suzuki@example.com' },
-  { name: '高橋 幸一', password: 'KoichiTakahashi789', email: 'koichi.takahashi@example.com' },
-  { name: '田中 美咲', password: 'MisakiTanaka101', email: 'misaki.tanaka@example.com' },
-  { name: '伊藤 明美', password: 'AkemiIto202', email: 'akemi.ito@example.com' },
-  { name: '山本 太郎', password: 'TaroYamamoto303', email: 'taro.yamamoto@example.com' },
-  { name: '中村 桃子', password: 'MomokoNakamura404', email: 'momoko.nakamura@example.com' },
-  { name: '小林 一郎', password: 'IchiroKobayashi505', email: 'ichiro.kobayashi@example.com' },
-  { name: '加藤 結衣', password: 'YuiKato606', email: 'yui.kato@example.com' },
-  { name: '吉田 雄大', password: 'YudaiYoshida707', email: 'yudai.yoshida@example.com' }
+  { username: '佐藤 健太', password: 'KentaSato123', email: 'kenta.sato@example.com' },
+  { username: '鈴木 花子', password: 'HanakoSuzuki456', email: 'hanako.suzuki@example.com' },
+  { username: '高橋 幸一', password: 'KoichiTakahashi789', email: 'koichi.takahashi@example.com' },
+  { username: '田中 美咲', password: 'MisakiTanaka101', email: 'misaki.tanaka@example.com' },
+  { username: '伊藤 明美', password: 'AkemiIto202', email: 'akemi.ito@example.com' },
+  { username: '山本 太郎', password: 'TaroYamamoto303', email: 'taro.yamamoto@example.com' },
+  { username: '中村 桃子', password: 'MomokoNakamura404', email: 'momoko.nakamura@example.com' },
+  { username: '小林 一郎', password: 'IchiroKobayashi505', email: 'ichiro.kobayashi@example.com' },
+  { username: '加藤 結衣', password: 'YuiKato606', email: 'yui.kato@example.com' },
+  { username: '吉田 雄大', password: 'YudaiYoshida707', email: 'yudai.yoshida@example.com' }
 ]
 
 # ユーザーデータをシードに挿入する
 users_data.each do |user_data|
   User.create!(
-    name: user_data[:username],
+    username: user_data[:username],
     password: user_data[:password],
     email: user_data[:email]
   )
@@ -26,5 +26,5 @@ end
 #
 # Examples:
 #
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+#   movies = Movie.create([{ username: 'Star Wars' }, { username: 'Lord of the Rings' }])
+#   Character.create(username: 'Luke', movie: movies.first)
