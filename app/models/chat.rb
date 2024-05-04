f27881ec4c7e9a432d957cc47475645b005a6c84
makoto-niko <mmana19970613@gmail.com>
2024-05-04 12:06:28 +0000

    chat通知仮機能

--- app/models/chat.rb
+++ app/models/chat.rb
@@ -1,20 +1,18 @@
 class Chat < ApplicationRecord
 belongs_to :user
 belongs_to :room
-has_many :notifications, as: :subject, dependent: :destroy
+  has_many :notifications, as: :notifiable, dependent: :destroy
   after_create_commit :create_notifications
 #after_create :create_notifications
 
 def create_notifications
   # 仮定: self.room.users は、このチャットのルームに属する全ユーザーを返します。
   # self.user_id は、このチャットを作成したユーザーのIDです。
-  self.room.users.where.not(id: self.user_id).each do |user|
+  self.room.user_rooms.where.not(user_id: self.user_id).each do |user|
     # ここで各ユーザーに対して通知を作成します。
     Notification.create!(
-      user: user,  # 通知を受けるユーザー
-      chat: self,  # 関連するチャット
-      action: 'new_message',  # 通知の種類
-      checked: false  # 未読状態
+      user_id: user.user_id,  # 通知を受けるユーザー
+      notifiable: self,  # 関連するチャット
     )
   end
 end
